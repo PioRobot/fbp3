ffad5c5419e5c78d62f4c03b3cfdabc3d14f4a72
PioRobot <90993966+PioRobot@users.noreply.github.com>
2023-07-12 19:54:13 -0600

    Add files via upload

---
+++ up.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('up', {
+init:function() {
+    this.mr()
+},
+mr:function(){
+window.addEventListener("keydown",function(e){
+    if (e.code=="W"){
+        
+    }
+    if (e.code=="A") {
+        
+    }
+    if (e.code=="D"){
+        
+    }
+})
+}
+})
